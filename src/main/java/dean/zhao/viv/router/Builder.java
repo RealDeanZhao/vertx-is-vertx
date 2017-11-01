79551c1b38599b5047f1e5e04431f1e4744a6bc6
Dean Zhao <zestxjest@outlook.com>
2017-11-01 10:10:31 +0800

    Handler

--- src/main/java/dean/zhao/viv/router/Builder.java
+++ src/main/java/dean/zhao/viv/router/Builder.java
@@ -1,25 +1,35 @@
 package dean.zhao.viv.router;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+import java.util.function.Consumer;
 
 public class Builder {
     static Router router;
-    private Builder(){}
-    public static Router build(Vertx vertx){
-        if (router == null){
-            router = Router.router(vertx);
+
+    private Builder() {
+    }
+
+    public static Router build(final Vertx vertx) {
+        if (null == router) {
+            synchronized (Builder.class) {
+                if (null == router) {
+                    router = Router.router(vertx);
+                }
+            }
         }
 
-        router.route().handler(routingContext -> {
-            HttpServerResponse response = routingContext.response();
+        final Consumer<RoutingContext> indexHandler = routingContext -> {
+            final HttpServerResponse response = routingContext.response();
             response.putHeader("conetent-type", "text/plain");
 
             response.end("Yo, lai kankan vertx");
-        });
+        };
+
+        router.route().handler(indexHandler::accept);
 
         return router;
     }
